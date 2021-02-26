e18370c4988f2579134de50e17bbb4969dbfb509
potew <potew0@gmail.com>
2021-02-26 19:16:59 -0300

    Ajustes CSS e event handlers

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Desafio da FacilitaPay. Possui um simples conversor de moedas em Vue.JS
 
 ## Instruções
-  Clone o repositório e rode o arquivo index.html dentro da pasta `\src`. Por ser um projeto de uma só página, não é necessário baixar/instalar nenhum arquivo adicional, sendo a biblioteca do Vue carregada via CDN no arquivo.
+  Clone o repositório e abra o arquivo index.html contido dentro da pasta `/src`. Por ser um projeto de uma só página, não é necessário baixar/instalar nenhum arquivo adicional, sendo a biblioteca do Vue carregada via CDN no arquivo.
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,6 @@ const rates = {
   USD: 5.2164,
   EUR: 6.3970,
   BRL: 1.000,
-  // eurusd = 1.2206,
   iof: 0.011,
   fx: 0.01
 }
@@ -10,8 +9,8 @@ const rates = {
 const EventHandler = {
   data() {
     return {
-      src_amount: 0.00,
-      converted_value: 0.00,
+      src_amount: null,
+      converted_value: null,
       iof_value: 0,
       fx_value: 0,
       src_currency: 'BRL',
@@ -22,6 +21,7 @@ const EventHandler = {
   methods: {
     setRate() {
       this.rate = rates[this.src_currency] / rates[this.dest_currency];
+      this.calculate();
     },
     calculate() {
       this.iof_value = this.src_amount * rates.iof;
